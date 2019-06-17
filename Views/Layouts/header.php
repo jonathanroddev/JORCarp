44635725e7e210fb8fb5641f1c52b0917f10c316
Jonathan <jonathan.rguez.mdza@gmail.com>
2019-06-17 21:57:26 +0100

    17-06-2019 21:57 - Estilos en el Header y Favicon

--- Views/Layouts/header.php
+++ Views/Layouts/header.php
@@ -5,11 +5,53 @@ include_once("../Models/DBUtils.php");
 ?>
 <head>
     <title><?php echo $title ?></title>
+    <link rel="shortcut icon" href="/Files/favicon.ico" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
 </head>
 
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <p class="font-weight-bolder font-italic"><a class="navbar-brand" href="#">Carpintería José Octavio</a></p>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <?php //if(isset($_SESSION["userStatus"]) && $_SESSION["userStatus"]==1){ ?>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><i class="material-icons">home</i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Importar Clientes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Facturas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Compras</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Balance</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Usuario
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Cuenta</a>
+                    <a class="dropdown-item" href="#">Logout<br><i class="material-icons">exit_to_app</i></a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <?php //}?>
+</nav>
 
 <div class="container">
     <div class="row justify-content-center align-items-center" style="height:50vh">
